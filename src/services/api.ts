3d212ac03005bcd190f4b09aa34073d2a71f7728
Romuelson <Romanch030@mail.ru>
2022-03-14 12:29:05 +0500

    Добавит поддержку токенов

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,23 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://9.react.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
 
-const createAPI = () => {
+const createAPI = (): AxiosInstance => {
 	const api = axios.create({
 		baseURL: BACKEND_URL,
 		timeout: REQUEST_TIMEOUT,
+	});
+
+	api.interceptors.request.use((config: AxiosRequestConfig) => {
+		const token = getToken();
+
+		if (token) {
+			config.headers['x-token'] = token;
+		}
+
+		return config;
 	});
 
 	return api;
