f82487fff8ff346b119f8ed3af887f2ff43df832
Romuelson <Romanch030@mail.ru>
2022-03-18 20:33:05 +0500

    Добавит описание тестового случая

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,6 +1,14 @@
+import { isArtistAnswerCorrect } from './game';
+import { makeFakeArtistQuestion } from './mocks/game';
+
+const mockArtistQuestion = makeFakeArtistQuestion();
+
 describe('Function: isArtistAnswerCorrect', () => {
 	it('should return "true" when answer is correct', () => {
-		// Тестовый случай
+		const { artist: correctAnswer } = mockArtistQuestion.song;
+		expect(isArtistAnswerCorrect(mockArtistQuestion, correctAnswer)).toBe(
+			true
+		);
 	});
 });
 
